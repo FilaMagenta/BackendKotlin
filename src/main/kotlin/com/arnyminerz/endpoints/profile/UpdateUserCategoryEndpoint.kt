61baa8f740f2edaad334830acbf807e9f45c2c4c
Arnau Mora <arnyminerz@proton.me>
2023-06-15 18:19:29 +0200

    Renamed permission
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/endpoints/profile/UpdateUserCategoryEndpoint.kt
+++ src/main/kotlin/com/arnyminerz/endpoints/profile/UpdateUserCategoryEndpoint.kt
@@ -14,7 +14,7 @@ import io.ktor.server.application.call
 import io.ktor.server.util.getValue
 import io.ktor.util.pipeline.PipelineContext
 
-object UpdateUserCategoryEndpoint : AuthenticatedEndpoint(Permissions.Users.UpdateCategory) {
+object UpdateUserCategoryEndpoint : AuthenticatedEndpoint(Permissions.Users.ChangeCategory) {
     override suspend fun PipelineContext<*, ApplicationCall>.endpoint(user: User) {
         val userId: Int by call.parameters
 
