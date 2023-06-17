fee40c9a2474b9dce76819d068db79f25dd88a86
Arnau Mora <arnyminerz@proton.me>
2023-06-17 14:52:37 +0200

    Fixed tests
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/Application.kt
+++ src/main/kotlin/com/arnyminerz/Application.kt
@@ -15,20 +15,26 @@ import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.doublereceive.DoubleReceive
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::installModules)
-        .start(wait = true)
+    embeddedServer(
+        Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = {
+            val isProduction = getEnvironmentPropertyOrVariable("config.production", "false").toBoolean()
+
+            val databaseInstance = if (isProduction) {
+                ProductionDatabase.Instance
+            } else {
+                DevelopmentDatabase.Instance
+            }
+            databaseInstance.set()
+
+            installModules()
+        }
+    ).start(wait = true)
 }
 
 fun Application.installModules() {
-    val isProduction = getEnvironmentPropertyOrVariable("config.production", "false").toBoolean()
-
-    val databaseInstance = if (isProduction) {
-        ProductionDatabase.Instance
-    } else {
-        DevelopmentDatabase.Instance
-    }
-    databaseInstance.set()
-
     install(DoubleReceive)
     configureStatusPages()
     configureJwt()
--- src/main/kotlin/com/arnyminerz/utils/ApplicationUtils.kt
+++ src/main/kotlin/com/arnyminerz/utils/ApplicationUtils.kt
@@ -7,10 +7,27 @@ val String.asEnvironmentVariable: String
         .map { if (it.isUpperCase()) "_${it.uppercase()}" else it.uppercase() }
         .joinToString("")
 
+/**
+ * Retrieves a configuration parameter provided by the `application.yaml` in resources, or if not present, takes from
+ * environment variable/property.
+ *
+ * Environment is considered before `application.yaml`, and naming follows:
+ * 1. For `application.yaml`, sub-properties are indicated with a `.`. For example: property `value` inside `config` is
+ *    obtained with `config.value`.
+ * 2. For environment values, the dot is substituted with a `_`, and all characters must be capital letters/numbers. For
+ *    example, the same case as before, as environment variable would be `CONFIG_VALUE`.
+ *
+ *    Also, capital letters are separated using `_`. This is because names are all case, naming can be confusing, so
+ *    `config.propertyValue` corresponds to `CONFIG_PROPERTY_VALUE`.
+ * @param key The key to fetch. See KtDoc for more information.
+ * @param default The default value to return if [key] is not present. If null, [IllegalArgumentException] will be
+ * thrown if [key] was not found.
+ * @throws IllegalArgumentException When [default] is null, and [key] was not found anywhere.
+ */
 fun Application.getEnvironmentPropertyOrVariable(key: String, default: String? = null): String =
-    environment.config.propertyOrNull(key)?.getString()
-        ?: System.getProperty(key)
+    System.getProperty(key)
         ?: System.getenv(key.asEnvironmentVariable)
+        ?: environment.config.propertyOrNull(key)?.getString()
         ?: default
         ?: throw IllegalArgumentException(
             "Could not find a property called \"%s\" in application.yaml, nor an environment variable called \"%s\""
